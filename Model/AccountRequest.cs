dda736e9ba8f80b1fbb6b9690294543c1401933a
Fabio <fabiocl93@gmail.com>
2024-10-26 15:01:56 +0100

    Add Request Models

---
+++ Model/AccountRequest.cs
@@ -0,0 +1,11 @@
+﻿using FinanceTracker.Infrastructure;
+
+namespace FinanceTracker.Model;
+
+public class AccountRequest
+{
+    public Guid AccountId { get; set; }
+    public string AccountName { get; set; }
+    public string AccountProvider { get; set; }
+    public string AccountType { get; set; }
+}
---
+++ Model/MonthlyRecordRequest.cs
@@ -0,0 +1,13 @@
+﻿namespace FinanceTracker.Model;
+
+public class MonthlyRecordRequest
+{
+    // The month of the record (1 = January, 12 = December)
+    public int Month { get; set; }
+
+    // The year of the record
+    public int Year { get; set; }
+
+    // The value of the account at the end of this month
+    public decimal Value { get; set; }
+}
