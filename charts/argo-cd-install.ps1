26ea34707e29c46327096b650431010714578e22
Fabio <fabiocl93@gmail.com>
2024-10-28 19:59:57 +0000

    Update Argos Helm Deployment

--- charts/argo-cd-install.ps1
+++ charts/argo-cd-install.ps1
@@ -2,6 +2,6 @@ helm repo add argo-cd https://argoproj.github.io/argo-helm
 
 helm dep update ./argo-cd/
 
-helm install argo-cd ./argo-cd/ --namespace argos-cd --create-namespace --wait
+helm install argo-cd ./argo-cd/ #--namespace argocd --create-namespace --wait
 
 kubectl port-forward svc/argo-cd-argocd-server 8080:443
