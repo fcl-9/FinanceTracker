d0d4d59b29d5691a3752f624b49631293f346a99
Fabio <fabiocl93@gmail.com>
2024-10-28 19:42:17 +0000

    Start Kubectl Proxy To Argos

--- charts/argo-cd-install.ps1
+++ charts/argo-cd-install.ps1
@@ -1,3 +1,7 @@
 helm repo add argo-cd https://argoproj.github.io/argo-helm
 
 helm dep update ./argo-cd/
+
+helm install argo-cd ./argo-cd/ --namespace argos-cd --create-namespace --wait
+
+kubectl port-forward svc/argo-cd-argocd-server 8080:443
